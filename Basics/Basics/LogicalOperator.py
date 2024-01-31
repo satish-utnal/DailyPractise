cba9da3de9e73cd05eb933c7a18de54fab7dc3cd
satish.utnal <satish.utnal@gmail.com>
2024-01-31 12:07:21 +0000

    Pandas practice

---
+++ Basics/Basics/LogicalOperator.py
@@ -0,0 +1,12 @@
+#Demo to show logical operator
+
+if 5>4 and 7>5:
+    print("True for and operator ")
+else:
+    print("False for and operator")
+
+
+if 5>4 or 7>9:
+    print("True for OR operator ")
+else:
+    print("False for OR operator")
