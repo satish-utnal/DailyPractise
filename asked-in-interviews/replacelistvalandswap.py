0cf5e2ea5d0b7b251559d0f059b843282f24b7b3
satish.utnal <satish.utnal@gmail.com>
2024-02-02 11:06:42 +0000

    updated example 16 from pandas

---
+++ asked-in-interviews/replacelistvalandswap.py
@@ -0,0 +1,12 @@
+my_list = [1,2,3,4,5,2]
+
+
+VAL = 2
+L1 = len(my_list)
+print(my_list, L1)
+my_list = [i for i in my_list if i != VAL]
+l2 = len(my_list)
+print(my_list,l2)
+for i in range(0, L1 - l2):
+    my_list.append('-')
+print(my_list, len(my_list))
