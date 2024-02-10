22b3f4fa23e4bbeabb560ba277b6dfae29b2f486
satish.utnal <satish.utnal@gmail.com>
2024-02-10 05:59:19 +0000

    added day 9 assignments

--- pandas_advanced/assignment11.py
+++ pandas_advanced/assignment11.py
@@ -1,5 +1,5 @@
 """
-10- write a program to get order ids and sales of return orders who's return reason is not known (nan)
+11- write a program to fill unknown(nan) return reason to a default value 'unknown'
 """
 
 import pandas as pd
@@ -12,5 +12,5 @@ returns_raw = pd.read_csv("../data/returns.txt")
 
 orders_returns = pd.merge(left=orders_raw, right=returns_raw, left_on="order_id", right_on="Order Id", how='left')
 orders_returns.drop(columns="Order Id", inplace=True)
-orders_nan = orders_returns[orders_returns['Return Reason'].notna()]
-print(orders_nan)
+
+orders_returns.fillna('unknown')
