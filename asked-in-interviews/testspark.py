f58dd24f9aec4aa521ef1534c9d50c25093cdc64
Satish Utnal <satish.utnal@gmail.com>
2024-02-19 10:26:22 +0000

    added pyspark test file

---
+++ asked-in-interviews/testspark.py
@@ -0,0 +1,26 @@
+# @Author Satish Utnal
+
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from pyspark.sql import SparkSession
+from datetime import datetime, date
+
+spark = SparkSession.builder.getOrCreate()
+
+rdd = spark.sparkContext.parallelize([
+    (1, 4., 'GFG1', date(2000, 8, 1), datetime(2000, 8, 1, 12, 0)),
+    (2, 8., 'GFG2', date(2000, 6, 2), datetime(2000, 6, 2, 12, 0)),
+    (3, 5., 'GFG3', date(2000, 5, 3), datetime(2000, 5, 3, 12, 0))
+])
+
+schema = ['a', 'b', 'c', 'd', 'e']
+
+df = spark.createDataFrame(rdd, schema=schema)
+
+df1 = spark.read.csv('../data/orders.txt', header=True, inferSchema=True)
+# DataFrame[order_id: string, order_date: string, customer_name: string, city: string, category: string, product_id: string, sales: string, profit: string
+
+df1.dtypes
